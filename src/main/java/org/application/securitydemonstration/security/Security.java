25124b4b27826dd0c47549cd506775f154db84f6
fedor <fediasosiskin@gmail.com>
2026-05-04 20:53:48 +0300

    First commit

--- src/main/java/org/application/securitydemonstration/security/Security.java
+++ src/main/java/org/application/securitydemonstration/security/Security.java
@@ -11,11 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -52,7 +47,6 @@ public class Security {
     public Security(JwtFilter jwtFilter) {
         this.jwtFilter = jwtFilter;
     }
-
 
 
 }
